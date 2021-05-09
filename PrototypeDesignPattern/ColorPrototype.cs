521ff5add4723a92626bbe96f35a67c459d526fc
Aya AbdulSlam <ahemaid@integrant.com>
2021-05-09 11:24:18 +0200

    Initial Create

---
+++ PrototypeDesignPattern/ColorPrototype.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrototypeDesignPattern
+{
+    internal abstract class ColorPrototype
+
+    {
+        public abstract ColorPrototype Clone();
+    }
+}
